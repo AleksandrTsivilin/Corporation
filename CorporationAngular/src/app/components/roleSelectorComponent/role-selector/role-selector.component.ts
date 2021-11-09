c070e1db3bd8f43c6d77fd2051ac064d9d87f493
AlexKherson <q0992358472@gmail.com>
2021-11-09 21:54:28 +0200

    added routing role-selector

--- CorporationAngular/src/app/components/roleSelectorComponent/role-selector/role-selector.component.ts
+++ CorporationAngular/src/app/components/roleSelectorComponent/role-selector/role-selector.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataUser } from 'src/app/interfaces/dataUser';
 
 @Component({
   selector: 'app-role-selector',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoleSelectorComponent implements OnInit {
 
-  constructor() { }
+
+  dataUser:DataUser={
+    id:null,
+    roles:null
+  }
+
+  isSelected:boolean=false;
+  modeSelector:string="";
+  constructor() {}
 
   ngOnInit(): void {
+    this.isSelected=false;
+    this.dataUser=this.getDataUser();
+  }
+
+  onSelect(selected:string){
+    console.log("onSelectedService");
+    this.modeSelector=selected;
+    this.isSelected=true;
+  }
+
+  //template methods
+
+  getDataUser():DataUser{
+    return {
+      id:1,
+      roles:["AdminManager","ProductManager"]
+    }
   }
 
 }
