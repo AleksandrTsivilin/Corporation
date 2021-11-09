bdf6d94f9b2e0bfde71a1ad9a27c3faf8e7f0e9a
AlexKherson <q0992358472@gmail.com>
2021-11-09 23:52:49 +0200

    added functionality admin-manager-component

--- CorporationAngular/src/app/components/adminPage/admin-manager/admin-manager.component.ts
+++ CorporationAngular/src/app/components/adminPage/admin-manager/admin-manager.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input} from '@angular/core';
 
 @Component({
   selector: 'app-admin-manager',
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminManagerComponent implements OnInit {
 
+  @Input () userId:number | null=null;
+  permissions:string[]=[];
+  modeAdminPage:string="";
+  isSelect:boolean=false;
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.userId)
+    if (this.userId !== null) {
+       this.permissions=this.getPermissions(this.userId);
+    }
+  }
+
+  canCreate():boolean{
+    return this.permissions.includes("create");
+  }
+
+  canGet():boolean{
+    return this.permissions.includes("read") || 
+      this.permissions.includes("update") || 
+      this.permissions.includes("delete");
+  }
+
+  onSelect(selected:string){
+    this.modeAdminPage=selected;
+    this.isSelect=true;
+  }
+
+  //template methods
+  getPermissions(userId:number):string[]{
+    return ["create","read","update","delete"]
   }
 
 }
