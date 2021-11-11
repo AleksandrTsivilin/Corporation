c543db0963152a64b4d81174654404a9e3a6110d
AlexKherson <q0992358472@gmail.com>
2021-11-11 16:44:17 +0200

    changed test data in admin-controller

--- CorporationApi/CorporationApi/Controllers/AdminController.cs
+++ CorporationApi/CorporationApi/Controllers/AdminController.cs
@@ -11,40 +11,40 @@ namespace CorporationApi.Controllers
     [ApiController]
     public class AdminController : ControllerBase
     {
-        [HttpGet("permissions")]
-        public IActionResult GetPermissionsByUser(int userId)
-        {
-            var permissions = new string[] { "create", "read", "delete", "update" };
-            return Ok(permissions);
-        }
+        //[HttpGet("permissions")]
+        //public IActionResult GetPermissionsByUser(int userId)
+        //{
+        //    var permissions = new string[] { "create", "read", "delete", "update" };
+        //    return Ok(permissions);
+        //}
 
-        [HttpGet("registrationData")]
-        public IActionResult GetRegistrationData(int userId)
-        {
-            var departmentsOfUser = new string[] { "dep 1", "dep 2" };
-            var allRoles = new string[] { "adminManager", "productManager" };
-            var allPermissions = new string[] { "create", "read", "update", "delete" };
-            var accessOfUser = new string[] { "full", "region", "factory", "department" };
-            return Ok(new {
-                departments = departmentsOfUser,
-                roles = allRoles,
-                permissions = allPermissions,
-                access = accessOfUser
-            });
-        }
+        //[HttpGet("registrationData")]
+        //public IActionResult GetRegistrationData(int userId)
+        //{
+        //    var departmentsOfUser = new string[] { "dep 1", "dep 2" };
+        //    var allRoles = new string[] { "adminManager", "productManager" };
+        //    var allPermissions = new string[] { "create", "read", "update", "delete" };
+        //    var accessOfUser = new string[] { "full", "region", "factory", "department" };
+        //    return Ok(new {
+        //        departments = departmentsOfUser,
+        //        roles = allRoles,
+        //        permissions = allPermissions,
+        //        access = accessOfUser
+        //    });
+        //}
 
-        [HttpPost("addUser")]
-        public IActionResult AddUser(RegistrationModel model)
-        {
-            return Ok();
-        }
+        //[HttpPost("addUser")]
+        //public IActionResult AddUser(RegistrationModel model)
+        //{
+        //    return Ok();
+        //}
 
-        [HttpGet("access")]
-        public IActionResult GetAccessByUser(int userId)
-        {
-            var accessOfUser = new string[] { "full", "region", "factory", "department" };
-            return Ok(new { access = accessOfUser });
-        }
+        //[HttpGet("access")]
+        //public IActionResult GetAccessByUser(int userId)
+        //{
+        //    var accessOfUser = new string[] { "full", "region", "factory", "department" };
+        //    return Ok(new { access = accessOfUser });
+        //}
 
         [HttpGet("users")]
         public IActionResult GetUsersByUser()
@@ -52,6 +52,7 @@ namespace CorporationApi.Controllers
             var userInfos = new List<UserInfo>{
                 new UserInfo
                 {
+                    Id=1,
                     Username="VasyaUser",
                     Firstname="Vasya",
                     Roles=new string[]{ "adminManager", "productManager" }
@@ -61,24 +62,31 @@ namespace CorporationApi.Controllers
 
             return Ok(userInfos);
         }
+
+        [HttpDelete]
+        public IActionResult Delete(int userId)
+        {
+            return Ok();
+        }
     }
 
-    public class RegistrationModel
-    {
-        public string FirstName { get; set; }
-        public string Username { get; set; }
-        public string Department { get; set; }
-        public string Role { get; set; }
-        public string[] Permissions { get; set; }
-        public string[] Access { get; set; }
-    }
+    //public class RegistrationModel
+    //{
+    //    public string FirstName { get; set; }
+    //    public string Username { get; set; }
+    //    public string Department { get; set; }
+    //    public string Role { get; set; }
+    //    public string[] Permissions { get; set; }
+    //    public string[] Access { get; set; }
+    //}
 
 
     public class UserInfo
     {
-        public string Username { get; set; } = "VasyaUser";
-        public string Firstname { get; set; } = "Vasya";
-        public string[] Roles { get; set; } = { "AdminManager", "ProductManager" };        
+        public int Id { get; set; }
+        public string Username { get; set; } 
+        public string Firstname { get; set; } 
+        public string[] Roles { get; set; }        
     }
 
     public class Permissions
