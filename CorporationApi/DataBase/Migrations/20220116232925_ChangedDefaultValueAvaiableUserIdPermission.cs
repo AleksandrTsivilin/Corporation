1bd18e6f5342a451c584d88eefe32d39ec431f14
AlexKherson <q0992358472@gmail.com>
2022-01-17 01:34:04 +0200

    added migration changed default value avaiableUserId in Permission

---
+++ CorporationApi/DataBase/Migrations/20220116232925_ChangedDefaultValueAvaiableUserIdPermission.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataBase.Migrations
+{
+    public partial class ChangedDefaultValueAvaiableUserIdPermission : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Permissions_AvaiablesUser_AvaiableUserId",
+                table: "Permissions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AvaiableUserId",
+                table: "Permissions",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Permissions_AvaiablesUser_AvaiableUserId",
+                table: "Permissions",
+                column: "AvaiableUserId",
+                principalTable: "AvaiablesUser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Permissions_AvaiablesUser_AvaiableUserId",
+                table: "Permissions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AvaiableUserId",
+                table: "Permissions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Permissions_AvaiablesUser_AvaiableUserId",
+                table: "Permissions",
+                column: "AvaiableUserId",
+                principalTable: "AvaiablesUser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
