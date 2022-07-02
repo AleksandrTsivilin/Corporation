3ad1619939a4b09dd78e26dd3c8ae557c2fa70b8
AlexKherson <q0992358472@gmail.com>
2022-07-02 17:36:44 +0300

    remove unnecessary code in nav-menu-component

--- CorporationAngular/src/app/components/mainPageComponent/nav-menu/nav-menu.component.ts
+++ CorporationAngular/src/app/components/mainPageComponent/nav-menu/nav-menu.component.ts
@@ -1,9 +1,8 @@
-import { Location } from '@angular/common';
-import { Component, HostListener, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
-import { ModeMainPageService, PageSizes } from 'src/app/services/modeMainPage/mode-main-page.service';
 
+import { Component, HostListener, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
+export const offsetHeader=30;
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
@@ -11,20 +10,13 @@ import { ModeMainPageService, PageSizes } from 'src/app/services/modeMainPage/mo
 })
 export class NavMenuComponent implements OnInit {
 
-
-  //isFullScreen:boolean=false;
-  
-  //pageSizes=PageSizes;
   isLogin:boolean=false;
   isFixed:boolean=false;
-  //activeNav:string="";
 
   
   constructor(
-    //private readonly modePageService:ModeMainPageService,
-    private readonly authService:AuthService,
-    //private readonly location:Location
-  ) { console.log("constr nav menu")}
+    private readonly authService:AuthService
+  ) { }
 
   ngOnInit(): void {
 
@@ -34,76 +26,15 @@ export class NavMenuComponent implements OnInit {
         : true; 
                  
     })
-
-    // this.modePageService.currentRouter$.subscribe(link=>{
-    //   this.activeNav=link;
-    // })
-    // this.modePageService.pageSize$.subscribe((mode)=>{
-    //   //this.isFullScreen=this.modePageService.isFullScreen;
-    //   //this.isFullScreen = mode===this.pageSizes.SHORT;
-    // })
   }
 
   @HostListener("document:scroll")
   scrollfunction(){
-    this.isFixed = window.pageYOffset >= 10;
-    //this.isHeaderShort = window.pageYOffset >= 80;
-    // console.log(document.documentElement.scrollTop)
-    // console.log(this.isHeaderShort)
-    // if (document.documentElement.scrollTop>80 && !this.isHeaderShort){      
-    //   try {
-    //     console.log("to go short header")
-    //     const errorField = this.renderer.selectRootElement('.anchor');
-    //     errorField.scrollIntoView({          
-    //       block: 'start'
-    //     });
-        
-    //     this.isHeaderShort=true;
-    //   } catch (err) {console.log("error")}
-
-    //   console.log('scrolling end ')
-      
-    // }  
-
-    // else{
-      
-    //   if (this.isHeaderShort && document.documentElement.scrollTop<300 && document.documentElement.scrollTop>81){
-    //     console.log('to go big header')
-    //     this.isHeaderShort=false;
-    //     try {
-    //       //console.log("to go short header")
-    //       const errorField = this.renderer.selectRootElement('.anchor');
-    //       errorField.scrollIntoView({          
-    //         block: 'end'
-    //       });
-          
-          
-    //     } catch (err) {console.log("error")}
-    //   }
-    // }
-    
-   
-      
+    this.isFixed = window.pageYOffset >= offsetHeader;  
   }  
 
-  // onFullScreen(){
-  //   this.modePageService.onFullScreen();
-  // }
-
-  // changeModePage(mode:number){
-  //   this.modePageService.pageSize$.next(mode);
-  // }
 
   toLogout(){
     this.authService.token$.next(null);
-    //this.changeModePage(this.pageSizes.LONG);
   }
-  // toMoveMain(){
-  //   this.modePageService.moveToMain();
-  // }
-
-  // toMoveLoginForm(){
-  //   this.modePageService.moveToLogin();
-  // }
-
 }
