8ad1ac1bf0b844684b77bc9ca9305b5e62d0df6c
AlexKherson <q0992358472@gmail.com>
2022-07-20 16:33:59 +0300

    removed unnecessary checking in interceptor

--- CorporationAngular/src/app/interceptors/auth.interceptor.ts
+++ CorporationAngular/src/app/interceptors/auth.interceptor.ts
@@ -14,13 +14,9 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private readonly authService:AuthService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler)
-  : Observable<HttpEvent<unknown>> {
+  : Observable<HttpEvent<unknown>> {  
+
     
-
-    const isAuth = this.authService.tokenData$.value !==null;
-
-    if (!isAuth) return next.handle(request);
-
     const copy = request.clone({withCredentials:true});
 
     return next.handle(copy);
